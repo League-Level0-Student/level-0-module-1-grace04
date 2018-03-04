01624576e655efaf61017be46ee357b67e95feef
grace04 <league@imac.attlocal.net>
2018-03-03 16:24:12 -0800

    MyAges

---
+++ src/MyAges.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class MyAges {
+	public static void main(String[] args) {
+		int x=0;
+		for(int i=0;i<14;i++){
+			JOptionPane.showMessageDialog(null, x++);
+		}
+	}
+}
